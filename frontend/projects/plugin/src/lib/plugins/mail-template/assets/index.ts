d8bd2163e8020cb575a3c8214a0c4d66fc8927ea
haiko van der schaaf <hvdschaaf@gmail.com>
2024-12-10 13:55:38 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	alfresco-authentication/backend/app/build.gradle
    #	alfresco-authentication/backend/gradle.properties
    #	alfresco-authentication/backend/plugin/build.gradle
    #	amsterdam-email-api/backend/app/build.gradle
    #	amsterdam-email-api/backend/app/src/main/kotlin/com/ritense/valtimo/amsterdam/emailapi/ApplicationReadyEventListener.kt
    #	amsterdam-email-api/backend/gradle.properties
    #	amsterdam-email-api/backend/plugin/build.gradle
    #	amsterdam-email-api/backend/plugin/gradle/publishing.gradle
    #	backend/alfresco-authentication/gradle/wrapper/gradle-wrapper.properties
    #	backend/alfresco-authentication/gradlew
    #	backend/alfresco-authentication/gradlew.bat
    #	backend/amsterdam-email-api/gradle/wrapper/gradle-wrapper.properties
    #	backend/amsterdam-email-api/gradlew
    #	backend/amsterdam-email-api/gradlew.bat
    #	backend/amsterdam-email-api/src/main/kotlin/com/ritense/valtimoplugins/amsterdam/emailapi/plugin/EmailApiAutoConfiguration.kt
    #	backend/amsterdam-email-api/src/main/kotlin/com/ritense/valtimoplugins/amsterdam/emailapi/plugin/EmailApiPlugin.kt
    #	backend/amsterdam-email-api/src/main/kotlin/com/ritense/valtimoplugins/amsterdam/emailapi/plugin/EmailApiPluginFactory.kt
    #	backend/berkelybridge-textgenerator/src/main/kotlin/com/ritense/valtimoplugins/berkelybridge/client/BerkelyBridgeClient.kt
    #	backend/berkelybridge-textgenerator/src/main/kotlin/com/ritense/valtimoplugins/berkelybridge/client/OpenRequestBody.kt
    #	backend/berkelybridge-textgenerator/src/main/kotlin/com/ritense/valtimoplugins/berkelybridge/plugin/BerkelyBridgeAutoConfiguration.kt
    #	backend/berkelybridge-textgenerator/src/main/kotlin/com/ritense/valtimoplugins/berkelybridge/plugin/BerkelyBridgePlugin.kt
    #	backend/berkelybridge-textgenerator/src/main/kotlin/com/ritense/valtimoplugins/berkelybridge/plugin/BerkelyBridgePluginFactory.kt
    #	berkelybridge-textgenerator/backend/app/build.gradle
    #	berkelybridge-textgenerator/backend/app/src/main/kotlin/com/ritense/valtimo/berkelybridge/ApplicationReadyEventListener.kt
    #	berkelybridge-textgenerator/backend/app/src/main/resources/bpmn/bb-html-test.bpmn
    #	berkelybridge-textgenerator/backend/app/src/main/resources/config/application.yml
    #	berkelybridge-textgenerator/backend/app/src/main/resources/config/pbac/all.role.json
    #	berkelybridge-textgenerator/backend/gradle.properties
    #	berkelybridge-textgenerator/backend/plugin/build.gradle
    #	berkelybridge-textgenerator/docker-compose.yml
    #	berkelybridge-textgenerator/docker/imports/old-keycloak/core-realm-valtimo.json
    #	berkelybridge-textgenerator/frontend/angular.json
    #	berkelybridge-textgenerator/frontend/package.json
    #	berkelybridge-textgenerator/frontend/projects/valtimo/berkelybridge/package.json
    #	berkelybridge-textgenerator/frontend/src/app/app.module.ts
    #	frontend/projects/valtimo-plugins/berkelybridge/src/lib/berkelybridge-textgenerator-plugin-module.ts
    #	frontend/projects/valtimo-plugins/freemarker/src/lib/plugins/mail-template/assets/index.ts
    #	frontend/projects/valtimo-plugins/spotler/src/lib/components/plugin-configuration/spotler-plugin-configuration.component.html
    #	frontend/src/environments/environment.ts
    #	gradle/wrapper/gradle-wrapper.properties

--- frontend/projects/plugin/src/lib/plugins/mail-template/assets/index.ts
+++ frontend/projects/plugin/src/lib/plugins/mail-template/assets/index.ts
@@ -13,5 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+apply plugin: "maven-publish"
 
-export * from './mail-template-plugin-logo';
+group = pluginGroupId
+version = pluginVersion
