59293e7b4364861f845571d9adf889d71ec448fc
haiko van der schaaf <hvdschaaf@gmail.com>
2025-09-18 14:24:34 +0200

    fix error

--- frontend/projects/plugin/src/lib/plugins/mail-template/assets/index.ts
+++ frontend/projects/plugin/src/lib/plugins/mail-template/assets/index.ts
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-apply plugin: "maven-publish"
 
-group = pluginGroupId
-version = pluginVersion
+export * from './mail-template-plugin-logo';
